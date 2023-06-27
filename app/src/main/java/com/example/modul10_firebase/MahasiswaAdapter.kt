1411113fc9f6e668ec8bb1c46f5a509dbd0dd781
armeisa ahda maulida <20102271@ittelkom-pwt.ac.id>
2023-06-27 22:15:07 +0700

    penambahan modul 11

--- app/src/main/java/com/example/modul10_firebase/MahasiswaAdapter.kt
+++ app/src/main/java/com/example/modul10_firebase/MahasiswaAdapter.kt
@@ -1,39 +1,73 @@
 package com.example.modul10_firebase
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
-class MahasiswaAdapter(private val userList: ArrayList<Mahasiswa>) : RecyclerView.Adapter<MahasiswaAdapter.MyViewHolder>() {
-//class mahasiswa adapter memupunyai variable userlist yg akan menampung data mahasiswa
-//yg berasal dari Mahasiswa.kt yg dalam bentuk array
-
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_layout, parent, false)
+@Suppress("DEPRECATION")
+class MahasiswaAdapter(
+    private val userList: ArrayList<Mahasiswa>,
+    private val context: Context,
+    private val editClickListener: (Mahasiswa) -> Unit,
+    private val deleteClickListener: (Mahasiswa) -> Unit) : RecyclerView.Adapter<MahasiswaAdapter.MyViewHolder>() {
+    private val database: DatabaseReference =
+        FirebaseDatabase.getInstance().reference.child("mahasiswa")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType:
+    Int): MyViewHolder {
+        val itemView =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_layout,
+                parent, false)
         return MyViewHolder(itemView)
     }
-    //untuk menampilkan data item layout (yg cardview)
-
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: MyViewHolder, position:
+    Int) {
         val currentItem = userList[position]
         holder.nama.text = currentItem.nama
-        holder.nim.text = currentItem.nim
+        holder.nim.text = "Nim: ${currentItem.nim}" // Ubah pengisian kolom nim
         holder.telp.text = currentItem.telp
+        holder.editButton.setOnClickListener {
+            editClickListener.invoke(currentItem)
+        }
+        holder.deleteButton.setOnClickListener {
+            deleteClickListener.invoke(currentItem)
+        }
     }
-    //inisialisasi nama berdasaran parameter yg ada di userList
-
     override fun getItemCount(): Int {
         return userList.size
     }
-    //untuk menghitung item listnya sesuai jumlah data yg ada difirebase
-
-    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val nama: TextView = itemView.findViewById(R.id.namaTextView)
+    inner class MyViewHolder(itemView: View) :
+        RecyclerView.ViewHolder(itemView) {
+        val nama: TextView =
+            itemView.findViewById(R.id.namaTextView)
         val nim: TextView = itemView.findViewById(R.id.nimTextView)
-        val telp: TextView = itemView.findViewById(R.id.telpTextView)
+        val telp: TextView =
+            itemView.findViewById(R.id.telpTextView)
+        val editButton: ImageButton =
+            itemView.findViewById(R.id.btnEdit)
+        val deleteButton: ImageButton =
+            itemView.findViewById(R.id.btnDellete)
+        init {
+            editButton.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    val mahasiswa = userList[position]
+                    editClickListener.invoke(mahasiswa)
+                }
+            }
+            deleteButton.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    val mahasiswa = userList[position]
+                    deleteClickListener.invoke(mahasiswa)
+                }
+            }
+        }
     }
-    //untuk mengarahkan misal variable nama diambil berdasarkan id nama textview yg ada di itemlayout(cardview)
 }
+
